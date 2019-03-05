49ce4abb823199f8a83a618d528e70a29a9573b9
waterlin <a10311036@gmail.com>
2019-03-05 21:23:16 +0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log(111)
+$('#fullpage').fullpage({
+        verticalCentered:false, // 取消垂直居中
+		navigation:true, // 显示导航圆点
+		//navigationTooltips:['','青年良品','多彩纤薄机身','一体化成型','5.5 英寸 1080P','64 位八核 CPU','双 4G 双卡双待','1300 万像素','3100mAh 典型容量','Android 5.1 内核',''], // 设置导航圆点提示信息
+		scrollingSpeed:700, // 设置滚动速度
+		//anchors:['','qingnian','duocai','yitihua','1080P','64CPU','dual4G','1300w','3100mAh','Android',''], // 设置锚链接
+		// 页面离开回调函数
+})
